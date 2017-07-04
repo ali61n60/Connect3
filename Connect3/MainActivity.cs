5d29f72fb3eced77bfbe5b9a27c22e0815e7b471
ali2 <ali62n62@yahoo.com>
2017-07-04 12:14:22 +0430

    player toggle in imageView click

--- Connect3/MainActivity.cs
+++ Connect3/MainActivity.cs
@@ -28,33 +28,50 @@ namespace Connect3
         private void initFields()
         {
             _image00 = FindViewById<ImageView>(Resource.Id.imageView00);
-            _image00.Click += _image00_Click;
+            _image00.Click += imamge_Click;
             _image01 = FindViewById<ImageView>(Resource.Id.imageView01);
-            _image01.Click += _image00_Click;
+            _image01.Click += imamge_Click;
             _image02 = FindViewById<ImageView>(Resource.Id.imageView02);
-            _image02.Click += _image00_Click;
+            _image02.Click += imamge_Click;
             _image10 = FindViewById<ImageView>(Resource.Id.imageView10);
-            _image10.Click += _image00_Click;
+            _image10.Click += imamge_Click;
             _image11 = FindViewById<ImageView>(Resource.Id.imageView11);
-            _image11.Click += _image00_Click;
+            _image11.Click += imamge_Click;
             _image12 = FindViewById<ImageView>(Resource.Id.imageView12);
-            _image12.Click += _image00_Click;
+            _image12.Click += imamge_Click;
             _image20 = FindViewById<ImageView>(Resource.Id.imageView20);
-            _image20.Click += _image00_Click;
+            _image20.Click += imamge_Click;
             _image21 = FindViewById<ImageView>(Resource.Id.imageView21);
-            _image21.Click += _image00_Click;
+            _image21.Click += imamge_Click;
             _image22 = FindViewById<ImageView>(Resource.Id.imageView22);
-            _image22.Click += _image00_Click;
+            _image22.Click += imamge_Click;
         }
-
-        private void _image00_Click(object sender, System.EventArgs e)
+        Player currentPlayer=Player.RedPlayer;
+        private void imamge_Click(object sender, System.EventArgs e)
         {
             ImageView imageView = (ImageView) sender;
             imageView.TranslationY = -1000f;
-            imageView.SetImageResource(Resource.Drawable.yellow);
+            int imageResource=0;
+            switch (currentPlayer)
+            {
+                    case Player.RedPlayer:
+                        imageResource = Resource.Drawable.red;
+                        currentPlayer=Player.YellowPlayer;
+                    break;
+                    case Player.YellowPlayer:
+                        imageResource = Resource.Drawable.yellow;
+                        currentPlayer =Player.RedPlayer;
+                    break;
+            }
+            imageView.SetImageResource(imageResource);
             imageView.Animate().TranslationYBy(1000f).Rotation(360).SetDuration(500);
-
         }
+    }
+
+    public enum Player
+    {
+        RedPlayer,
+        YellowPlayer
     }
 }
 
